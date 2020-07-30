0e06a3a4194b9460e93d0d5ecf1bc273de2ec713
gao <89266498@qq.com>
2020-07-30 21:28:31 +0800

    sql富发

---
+++ Huanbao/123.sql
@@ -0,0 +1,5 @@
+INSERT INTO 123 ('C_NOX_SC','C_NOX','C_SO2_SC','C_SO2','C_YC_SC','C_YC','C_O2','F_gas') VALUES ('0','0','2.57','2.57','0','0','21.27','302688');
+INSERT INTO 123 ('C_NOX_SC','C_NOX','C_SO2_SC','C_SO2','C_YC_SC','C_YC','C_O2','F_gas') VALUES ('0','0','2.57','2.57','0','0','21.27','302688');
+INSERT INTO 123 ('C_NOX_SC','C_NOX','C_SO2_SC','C_SO2','C_YC_SC','C_YC','C_O2','F_gas') VALUES ('0','0','2.57','2.57','0','0','21.27','302688');
+INSERT INTO 123 ('C_NOX_SC','C_NOX','C_SO2_SC','C_SO2','C_YC_SC','C_YC','C_O2','F_gas') VALUES ('0','0','2.57','2.57','0','0','21.27','302688');
+INSERT INTO 123 ('C_NOX_SC','C_NOX','C_SO2_SC','C_SO2','C_YC_SC','C_YC','C_O2','F_gas') VALUES ('0','0','2.57','2.57','0','0','21.27','302688');
